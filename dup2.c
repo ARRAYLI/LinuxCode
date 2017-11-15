ff221f6fa6eba7d9f5b7b4c8d1e5f1ae6a7a4f2e
ARRAYLI <32920845+ARRAYLI@users.noreply.github.com>
2017-11-15 20:59:04 +0800

    dup2 复制文件描述
    dup2 复制文件描述

---
+++ dup2.c
@@ -0,0 +1,46 @@
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void)
+{
+	int fd =open("english.txt",O_RDWR);
+	if( fd == -1 )
+	{
+		perror("open");
+		exit(1);
+	}
+
+	int fd1 =open("a.txt",O_RDWR);
+	if( fd1 == -1 )	
+	{
+		perror("open");
+		exit(1);
+	}
+
+	printf("fd = %d\n",fd);
+	printf("fd1 = %d\n",fd1);
+
+	int ret = dup2(fd1, fd);
+	if( ret == -1 )
+	{	
+		perror("dup2");
+		exit(1);
+	}
+
+	printf(" current fd = %d\n",ret);
+
+	char *buf = "主要看气质 !!!!!!!!!!!!!!!!!\n";
+
+	write(fd,buf,strlen(buf));
+	write(fd1,"hello world!",12);
+
+	close(fd);
+	close(fd1);
+
+	return 0;
+}
